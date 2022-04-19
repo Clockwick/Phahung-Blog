edc92be5b79af77f5d25a05acb9a5935ef1ce7bb
It5Me <56817602+It5Me@users.noreply.github.com>
2022-04-20 02:10:53 +0700

    EDIT : EditAnnouncement canEdit

--- packages/apps/phahung-admin/src/pages/Announcements/EditAnnouncement/EditAnnouncement.tsx
+++ packages/apps/phahung-admin/src/pages/Announcements/EditAnnouncement/EditAnnouncement.tsx
@@ -1,18 +1,33 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { Button, Input } from '@chan-chala/uikit';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import mockAnnouncement from 'mock/announcements';
+import { IEditAnnouncementPayload } from './types';
 import { useParams } from 'react-router-dom';
+// import announcementApiCall from 'api/Announcement/announcement';
 
 interface Params {
   announcementId: string;
 }
 const EditAnnouncement: React.FC = () => {
-  const response = mockAnnouncement[0];
   const { announcementId } = useParams<Params>();
+  const response = mockAnnouncement[parseInt(announcementId, 10) - 1];
   const [title, setTitle] = useState<string>(response.title);
   const [description, setDescription] = useState<string>(response.description);
-  const handleOnClick = () => {};
+  const handleOnClick = () => {
+    const payload: IEditAnnouncementPayload = {
+      id: response.id,
+      title,
+      description,
+    };
+    console.log('payload', payload);
+  };
+
+  // useEffect(() => {
+  //   announcementApiCall.getAnnouncementById(announcementId).then((res) => {
+  //     initialDataAnnouncement = res.data;
+  //   });
+  // });
 
   return (
     <>
