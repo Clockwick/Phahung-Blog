d1bd423d5293b77fb63769a9f19ebf24f9334879
arthris <oclockth@gmail.com>
2022-04-23 14:05:26 +0700

    Signout if 401

--- packages/apps/phahung-admin/src/utils/api.ts
+++ packages/apps/phahung-admin/src/utils/api.ts
@@ -9,20 +9,19 @@ export default <T>(config: AxiosRequestConfig): Promise<AxiosResponse<T>> => {
     (response) => {
       return response;
     },
-    () => {
-      // if (error.response.status === 401) {
-      //   const localAdminJson =
-      //     localStorage.getItem('persist:chan-chara-admin') ?? '';
-      //   if (localAdminJson.length > 0) {
-      //     const { isLoggedIn } = JSON.parse(JSON.parse(localAdminJson).user);
-      //     if (isLoggedIn) {
-      //       localStorage.removeItem('persist:chan-chara-admin');
-      //       localStorage.removeItem('idToken');
-      //       window.location.reload();
-      //     }
-      //   }
-      // }
-      // return error;
+    (error) => {
+      if (error.response.status === 401) {
+        const localAdminJson = localStorage.getItem('phahung-admin') ?? '';
+        if (localAdminJson.length > 0) {
+          const { isLoggedIn } = JSON.parse(JSON.parse(localAdminJson).user);
+          if (isLoggedIn) {
+            localStorage.removeItem('phahung-admin');
+            localStorage.removeItem('idToken');
+            window.location.reload();
+          }
+        }
+      }
+      return error;
     },
   );
 
