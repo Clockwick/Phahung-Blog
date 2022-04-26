202861609bae63abd807cb5022149a08c02c5080
arthris <oclockth@gmail.com>
2022-04-27 03:39:55 +0700

    signin bg

--- packages/apps/phahung/src/pages/Signin.tsx
+++ packages/apps/phahung/src/pages/Signin.tsx
@@ -95,8 +95,7 @@ const Signin: React.FC = () => {
           sm={4}
           md={7}
           sx={{
-            backgroundImage:
-              'url(https://images.unsplash.com/photo-1595495529260-5d01d0271714?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80)',
+            backgroundImage: 'url(/assets/images/bg.png)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light'
